648d5f285ee9514413f7463b161c67ae1cf2404c
peter <ptmartinezsound@gmail.com>
2021-05-06 10:54:21 -0500

    voting system

--- models/User.js
+++ models/User.js
@@ -1,9 +1,16 @@
 const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-class User extends Model { }
+// create our User model
+class User extends Model {
+    // set up method to run on instance data (per user) to check password
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
-// define table columns and configuration
+// create fields/columns for User model
 User.init(
     {
         id: {
@@ -33,6 +40,18 @@ User.init(
         }
     },
     {
+        hooks: {
+            // set up beforeCreate lifecycle "hook" functionality
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
         sequelize,
         timestamps: false,
         freezeTableName: true,
@@ -42,3 +61,48 @@ User.init(
 );
 
 module.exports = User;
+
+// const { Model, DataTypes } = require('sequelize');
+// const sequelize = require('../config/connection');
+
+// class User extends Model { }
+
+// // define table columns and configuration
+// User.init(
+//     {
+//         id: {
+//             type: DataTypes.INTEGER,
+//             allowNull: false,
+//             primaryKey: true,
+//             autoIncrement: true
+//         },
+//         username: {
+//             type: DataTypes.STRING,
+//             allowNull: false
+//         },
+//         email: {
+//             type: DataTypes.STRING,
+//             allowNull: false,
+//             unique: true,
+//             validate: {
+//                 isEmail: true
+//             }
+//         },
+//         password: {
+//             type: DataTypes.STRING,
+//             allowNull: false,
+//             validate: {
+//                 len: [4]
+//             }
+//         }
+//     },
+//     {
+//         sequelize,
+//         timestamps: false,
+//         freezeTableName: true,
+//         underscored: true,
+//         modelName: 'user'
+//     }
+// );
+
+// module.exports = User;
